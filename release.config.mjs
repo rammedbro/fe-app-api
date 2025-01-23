e4b5bbb4d8039140ebbb1f45570f614c8062895c
Ramil Sharipov <rammedbro@yandex.ru>
2025-01-23 13:48:37 +0300

    chore(FAA-18): Fix releases
    * Fixed typo in conventionalcommits preset
    * Added missing preset package
    * Refactor ci jobs

--- release.config.mjs
+++ release.config.mjs
@@ -3,7 +3,7 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 export default {
-  preset: 'conventional-commits',
+  preset: 'conventionalcommits',
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
