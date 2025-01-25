89583a7a714aeba78c64bd12b11f89c853bb8078
Ramil Sharipov <rammedbro@yandex.ru>
2025-01-25 18:04:01 +0000

    ci(FAA-19): Set missing options for releases

--- release.config.mjs
+++ release.config.mjs
@@ -4,11 +4,17 @@
  */
 export default {
   preset: 'conventionalcommits',
+  branches: ['master', 'next'],
   plugins: [
     '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    ['@semantic-release/gitlab'],
-    ['semantic-release-jira-notes', { jiraHost: 'imolater.atlassian.net' }],
+    '@semantic-release/gitlab',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
   ],
 };
